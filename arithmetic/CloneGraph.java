6d70d82fda5224222097bf803fee082a1796f3db
lk <2322604533@qq.com>
2023-05-22 18:24:26 +0800

    1

--- arithmetic/CloneGraph.java
+++ arithmetic/CloneGraph.java
@@ -2,10 +2,12 @@ package zeus.arithmetic;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * 克隆一份地图
+ * 给你无向连通图中一个节点的引用，请你返回该图的深拷贝（克隆）。
  */
 // L133
 public class CloneGraph {
@@ -13,7 +15,8 @@ public class CloneGraph {
     // Definition for a Node.
     class Node {
         public int val;
-        public List<Node> neighbors;
+        public List<Node> neighbors; // 相邻的结点
+
         public Node() {
             this.val = 0;
             neighbors = new ArrayList<Node>();
@@ -28,11 +31,46 @@ public class CloneGraph {
         }
     }
 
-    public Node CloneGraph() {
-        return null;
+    // 获得一个内部类，或者将内部类改成静态内部类
+    public Node newNode() {
+        return new Node();
     }
 
-    public static void main(String[] args) {
-        CloneGraph obj = new CloneGraph();
+    /**
+     * 为了防止多次遍历同一个节点，陷入死循环，
+     * 我们需要用一种数据结构记录已经被克隆过的节点。
+     * 利用哈希表
+     * @param node
+     * @return
+     */
+    public Node cloneGraph(Node node) {
+        // 使用 visited 记录被访问过的结点，以免造成死循环
+        Map<Node, Node> visited = new HashMap<>();
+        return dfs(node, visited);  // return clonNode
+    }
+
+    /**
+     * Map<Node,Node> 前者为原始结点，后者为克隆的结点
+     * @param node
+     * @param visited
+     * @return
+     */
+    private Node dfs(Node node, Map<Node, Node> visited) {
+        if (node == null) return null;
+
+        // 如果原图的结点已经被访问过，则直接从哈希表中取出对应的结点返回
+        if (visited.containsKey(node)) return visited.get(node);
+
+        // 对结点进行克隆，每次克隆前都new一个链表来存储邻接点的相邻结点
+        Node clonNode = new Node(node.val, new ArrayList<>());
+
+        // 用哈希表未被访问的原结点和被克隆的结点
+        visited.put(node, clonNode);
+
+        // 遍历原图该结点的邻接点，并且更新邻接结点的列表
+        for (Node n : node.neighbors) {
+            clonNode.neighbors.add(dfs(n, visited));
+        }
+        return clonNode;
     }
 }
---
+++ arithmetic/MaxSlidingWindow.java
@@ -0,0 +1,40 @@
+package zeus.arithmetic;
+
+import java.util.PriorityQueue;
+
+public class MaxSlidingWindow {
+
+    /**
+     * 对于当前的队列而言，可以使用O(K)的时间遍历其中的每一个元素
+     * 在k个元素当中找到最大值，因此结果的数组中有(n-k+1)，算法时间复杂度为O((n-k+1)k)
+     * 但是会超时，因此可以想到两个相邻的窗口只差了一个元素，他们共用这(k-1)个元素，只有一个元素是变化的
+     * 因此可以联想到结点只有 k 个的大根堆 ，也叫作[优先队列]，每一次移动都是对大根堆的添加和移除操作
+     * 当最大值在滑动窗口的左边时，如 1，[3，-1，-3]，5，3，6，7 => 1，3，[-1，-3，5]，3，6，7
+     * 将 3 移除堆中，然后调整堆的结构，保证这是大根堆
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b)->(b[1] - a[1]));
+
+        int[] res = new int[nums.length - k + 1];
+
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            maxHeap.add(new int[]{i, nums[i]});
+
+            if (i >= k - 1) {
+
+                while (maxHeap.peek()[0] <= i - k) maxHeap.poll();
+
+                res[index++] = maxHeap.peek()[1];
+            }
+        }
+
+        return res;
+    }
+}
